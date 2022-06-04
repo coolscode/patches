215e8305bc2486d4ddc07543208b50dd2f42c57c
xiaoleGun <1592501605@qq.com>
2022-06-04 09:28:48 +0800

    Add apply patches script
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Initial patches for AOSP-Krypton
+# Patches
+
+## AOSP-Krypton 
+- Adapt MTK devices
+- More...
+
+First, please synchronize the source code of AOSP krypton
+
+```shell
+cd sourcedirectory
+```
+
+Then apply the patches
+
+```shell
+bash apply.sh (kosp)[Optional]
+```
+
+There may be more in the future...
---
+++ apply.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+PD=$PWD/patches
+
+apply_patch() {
+patches="$(readlink -f -- $1)"
+tree="$2"
+
+for project in $(cd $patches/patches/$tree; echo *);do
+	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
+	pushd $p
+	for patch in $patches/patches/$tree/$project/*.patch;do
+		git am $patch || exit
+	done
+	popd
+done
+}
+
+if [ $1 = kosp ];then
+echo ""
+echo "Adapt AOSP Krypton to MTK"
+echo "Executing in 5 seconds - CTRL-C to exit"
+echo ""
+fi
+
+sleep 5
+
+echo "Applying patches"
+apply_patch $PD $1
